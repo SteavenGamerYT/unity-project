2a4a81197fd2bb3f2078cfed42d0ff1dcacb790f
SteavenDev <omartube706@gmail.com>
2020-10-13 02:24:41 +0200

    Uploaded an old game to edit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour {
+
+	#region Variables
+	#endregion
+
+	#region Unity Methods
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void PlayGame2()
+	{
+		SceneManager.LoadScene(2);
+	}
+	public void PlayGame3()
+	{
+		SceneManager.LoadScene(3);
+	}
+	public void PlayGame4()
+	{
+		SceneManager.LoadScene(4);
+	}
+	public void PlayGame5()
+	{
+		SceneManager.LoadScene(5);
+	}
+	public void PlayGame6()
+	{
+		SceneManager.LoadScene(6);
+	}
+	#endregion
+}
---
+++ Assets/Scripts/PlayertMovemet.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class PlayertMovemet : MonoBehaviour {
+
+    #region Variables
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    Vector3 Velocity;
+    bool isGrounded;
+    #endregion
+
+    #region Unity Methods
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && Velocity.y < 0)
+		{
+            Velocity.y = -2f;
+		}
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed *Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+		{
+            Velocity.y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+		}
+
+        Velocity.y += Physics.gravity.y * Time.deltaTime;
+
+        controller.Move(Velocity * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            TakeDamage(5);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            TakeDamage(-5);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(-100);
+        }
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+
+        healthBar.SetHealth(currentHealth);
+    }
+    #endregion
+}
